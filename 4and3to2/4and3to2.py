3ea99b1d448ca2f951741b171e8f1bd6fe89e02b
MarsPain <1434579002@qq.com>
2018-03-25 22:06:28 +0800

    对2字词、3字词、4字词的处理，完成了2字词、3字词的词义判断、4字词的基础划分部分，下一步要完善3字词满足“动名名”时进行拆分的部分和4字词根据与3字词的编辑距离进行转换的部分

--- 4and3to2/4and3to2.py
+++ 4and3to2/4and3to2.py
@@ -2,10 +2,13 @@ import pandas as pd
 import csv
 import numpy as np
 import re
+import jieba.posseg as pseg
+
 
 data = pd.read_csv("data_init.csv", delimiter="\t")    #用\t而不是,做分隔符
 length = data.shape[0]
 data.insert(2, "Usage", None)
+data.insert(4, "Effect", None)
 
 #对性味部分数据进行处理
 for i in range(length):
@@ -13,4 +16,53 @@ for i in range(length):
     data["Taste"].loc[i] = re.split("[，、]", L[0])
     data["Usage"].loc[i] = re.split("[，、]", L[1])
 # print(data)
+
+#对功用主治部分进行处理
+for i in range(length):
+    L = data["Function"].loc[i].split("。")
+    data["Function"].loc[i] = re.split("[，、]", L[0])
+    # data["Effect"].loc[i] = L[1]
+
+#对功用部分进行分词处理
+set2 = set()
+set3 = set()
+set4 = set()
+for i in range(length):
+    listFunction = data["Function"].loc[i]
+    length2 = len(listFunction)
+
+    for j in range(length2):
+        if len(listFunction[j]) == 2:
+            set_temp = {listFunction[j]}
+            temp = set2.isdisjoint(set_temp)
+            if temp:
+                pass
+            else:
+                set2.add(listFunction[j])
+
+        elif len(listFunction[j]) == 3:
+            word_list = []
+            char_list = []
+            for s in listFunction[j]:
+                word = pseg.cut(s)
+                for w in word:
+                    word_list.append(w.word)
+                    char_list.append(w.flag)
+            # print(char_list)
+            if char_list == ['v', 'n', 'n']:
+                pass
+            else:
+                set3.add(listFunction[j])
+
+        elif len(listFunction[j]) == 4:
+            words = re.findall('.{2}', listFunction[j])
+            set_temp = set(words)
+            temp = set4.isdisjoint(set_temp)
+            if temp:
+                listFunction[j] = words
+                set2.add(words[0])
+                set2.add(words[1])
+            else:
+                set4.add(listFunction[j])
+
 data.to_csv("data_treat.csv")
--- 4and3to2/test.py
+++ 4and3to2/test.py
@@ -1,9 +1,37 @@
-s = "123我，456你"
-import re
+# s = "123我，456你"
+# import re
+#
+# s = re.sub('，', ' ',s)
+# s = re.sub('我', ' ' , s)
+# s = re.sub()
+# s = re.split('\s+', s)
+# print(s)
+# print(type(s))
+#
+# s = {1, 2}
+# s2 = {1}
+# s3 = ["a", "s"]
+# s4 = {'a', 's'}
+#
+# # temp1 = s2.isdisjoint((s))
+# # print(temp1)
+# # s.add(1)
+# # print(s)
+# set = set(s3)
+# print(type(set))
+# print(set)
+# print(s4)
 
-s = re.sub('，', ' ',s)
-s = re.sub('我', ' ' , s)
-s = re.sub()
-s = re.split('\s+', s)
-print(s)
-print(type(s))
+import jieba.posseg as pseg
+words = pseg.cut("祛")
+for i in words:
+    print(i.word, i.flag)
+    print(type(i.flag))
+
+# s = "祛风寒"
+# for i in s:
+#     print(i)
+#
+# l1 = ['我', '是']
+# l2 = ['我', '是']
+# print(l1 == l2)
