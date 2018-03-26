e2b9aecd28c02651cc3c68e4721b923b8e9d5f43
MarsPain <1434579002@qq.com>
2018-03-26 10:40:19 +0800

    添加计算最小编辑距离的方法

--- 4and3to2/4and3to2.py
+++ 4and3to2/4and3to2.py
@@ -66,4 +66,29 @@ for i in range(length):
             else:
                 set4.add(listFunction[j])
 
+#最小编辑距离计算方法
+def difflib_leven(str1, str2):
+    len_str1 = len(str1) + 1
+    len_str2 = len(str2) + 1
+    #创建矩阵
+    matrix = [0 for n in range(len_str1 * len_str2)]
+    #初始化X轴
+    for i in range(len_str1):
+      matrix[i] = i
+    #初始化Y轴
+    for j in range(0, len(matrix), len_str1):
+      if j % len_str1 == 0:
+          matrix[j] = j // len_str1
+
+    for i in range(1, len_str1):
+      for j in range(1, len_str2):
+          if str1[i-1] == str2[j-1]:
+              cost = 0
+          else:
+              cost = 1
+          matrix[j*len_str1+i] = min(matrix[(j-1)*len_str1+i]+1,
+                                      matrix[j*len_str1+(i-1)]+1,
+                                      matrix[(j-1)*len_str1+(i-1)] + cost)
+    return matrix[-1]
+
 data.to_csv("data_treat.csv")
--- 4and3to2/test.py
+++ 4and3to2/test.py
@@ -41,3 +41,33 @@
 # # s = re.split("[，]", s)
 # s = re.sub("，", "", s)
 # print(s)
+
+import difflib
+def difflib_leven(str1, str2):
+    len_str1 = len(str1) + 1
+    len_str2 = len(str2) + 1
+    #create matrix
+    matrix = [0 for n in range(len_str1 * len_str2)]
+    #init x axis
+    for i in range(len_str1):
+      matrix[i] = i
+    #init y axis
+    for j in range(0, len(matrix), len_str1):
+      if j % len_str1 == 0:
+          matrix[j] = j // len_str1
+
+    for i in range(1, len_str1):
+      for j in range(1, len_str2):
+          if str1[i-1] == str2[j-1]:
+              cost = 0
+          else:
+              cost = 1
+          matrix[j*len_str1+i] = min(matrix[(j-1)*len_str1+i]+1,
+                                      matrix[j*len_str1+(i-1)]+1,
+                                      matrix[(j-1)*len_str1+(i-1)] + cost)
+    return matrix[-1]
+
+s1 = "补益元气"
+s2 = "补元气"
+distance = difflib_leven(s1, s2)
+print(distance)
