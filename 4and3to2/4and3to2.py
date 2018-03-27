c6923d3cc80e978cee9c923d24f5ff80af11250f
MarsPain <1434579002@qq.com>
2018-03-27 22:14:22 +0800

    对结果数据进行分析和导出

--- 4and3to2/4and3to2.py
+++ 4and3to2/4and3to2.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 def get_data():
     data = pd.read_csv("data_init.csv", delimiter="\t")    #用\t而不是,做分隔符
     length = data.shape[0]
-    #插入列
+    #插入列，分别保存“归经”和“主治”的数据
     data.insert(2, "Type", None)
     data.insert(4, "Effect", None)
 
@@ -28,8 +28,14 @@ def get_data():
 #对功效部分的数据进行分词处理
 def word_cut_function(data, length):
     set2 = {}
-    set3 = {}
-    set4 = {}
+    #保存可以拆分的3字词
+    set3_true = {}
+    # 保存不可拆分的3字词
+    set3_false = {}
+    # 保存可以拆分的4字词
+    set4_true = {}
+    # 保存不可拆分的4字词
+    set4_false = {}
 
     for i in range(length):
         listFunction = data["Function"].loc[i]
@@ -42,15 +48,15 @@ def word_cut_function(data, length):
 
         for j in range(length2):
             if len(listFunction[j]) == 3:
-                word= word_cut_3(set2, set3, listFunction[j])
+                word= word_cut_3(set2, set3_true, set3_false, listFunction[j])
                 listFunction[j] = word
 
         for j in range(length2):
             if len(listFunction[j]) == 4:
-                word = word_cut_4(set2, set3, set4, listFunction[j])
+                word = word_cut_4(set2, set3_true, set3_false, set4_true, set4_false, listFunction[j])
                 listFunction[j] = word
 
-    return data, set2, set3, set4
+    return data, set2, set3_true, set3_false, set4_true, set4_false
 
 #对主治部分的数据进行分词处理
 # def word_cut_effect(data, length):
@@ -77,9 +83,10 @@ def word_cut_function(data, length):
 #     return data, set2, set3, set4
 
 def word_cut_2(set2, word):
+    #保存到词库并计数
     set2[word] = (set2[word] if word in set2 else 0) + 1
 
-def word_cut_3(set2, set3, word):
+def word_cut_3(set2, set3_true, set3_false, word):
     #用两个列表记录单字及其词性
     word_list = []
     char_list = []
@@ -92,6 +99,8 @@ def word_cut_3(set2, set3, word):
             char_list.append(w.flag)
     #根据三字词中每个字的词性进行进一步处理
     if char_list == ['v', 'n', 'n']:
+        #若满足“动名名”的规律，则分配到set3_true词库中
+        set3_true[word] = (set3_true[word] if word in set3_true else 0) + 1
         #字符串拼接
         word1 = '%s%s' % (word_list[0], word_list[1])
         word2 = '%s%s' % (word_list[0], word_list[2])
@@ -101,10 +110,11 @@ def word_cut_3(set2, set3, word):
         set2[word2] = (set2[word2] if word2 in set2 else 0) + 1
         return word
     else:
-        set3[word] = (set3[word] if word in set3 else 0) + 1
+        #无法拆分，先分配到set3_false词库中
+        set3_false[word] = (set3_false[word] if word in set3_false else 0) + 1
         return word
 
-def word_cut_4(set2, set3, set4, word):
+def word_cut_4(set2, set3_true, set3_flase, set4_true, set4_false, word):
     #与set2中的2字词进行对比并处理
     #按照长度2进行分割
     word_list = re.findall('.{2}', word)
@@ -114,23 +124,27 @@ def word_cut_4(set2, set3, set4, word):
             temp = True
         else:
             pass
+
     if temp:
+        # 若22拆分后的单词在2字词库中出现过
+        set4_true[word] = (set4_true[word] if word in set4_true else 0) + 1
         set2[word_list[0]] = (set2[word_list[0]] if word_list[0] in set2 else 0) + 1
         set2[word_list[1]] = (set2[word_list[1]] if word_list[1] in set2 else 0) + 1
         word = '%s %s' % (word_list[0], word_list[1])
         return word
     else:
-        #与set3中的3字词进行对比并处理
-        for word_3 in set3:
+        #若22拆分后的单词在2字词库中未出现过，则先与set3_true中的3字词进行对比
+        for word_3 in set3_true:
             dis = difflib_leven(word_3, word)
             if dis == 1:
-                word = word_cut_3(set2, set3, word_3)
+                # 若与set3_true中某个3字词只有一个编辑距离
+                word = word_cut_3(set2, set3_true, set3_flase, word_3)
                 return word
             else:
-                set4[word] = (set4[word] if word in set4 else 0) + 1
+                set4_false[word] = (set4_false[word] if word in set4_false else 0) + 1
                 return word
 
-#用动态规划对编辑距离进行计算
+#用动态规划对字符串之间的编辑距离进行计算
 def difflib_leven(str1, str2):
     len_str1 = len(str1) + 1
     len_str2 = len(str2) + 1
@@ -155,18 +169,24 @@ def difflib_leven(str1, str2):
                                       matrix[(j-1)*len_str1+(i-1)] + cost)
     return matrix[-1]
 
-def data_analyse(set2, set3, set4):
-    print("2字词库")
+def data_analyse(set2, set3_true, set3_false, set4_true, set4_false):
+    print("==========2字词库==========")
     for i in set2:
         print(i, set2[i])
-    print("3字词库")
-    for i in set3:
-        print(i, set3[i])
-    print("需要人工处理的4字词")
-    for i in set4:
-        print(i, set4[i])
+    print("==========被拆分的3字词库==========")
+    for i in set3_true:
+        print(i, set3_true[i])
+    print("==========未被拆分的3字词库==========")
+    for i in set3_false:
+        print(i, set3_false[i])
+    print("==========被拆分的4字词==========")
+    for i in set4_true:
+        print(i, set4_true[i])
+    print("==========未被拆分的4字词==========")
+    for i in set4_false:
+        print(i, set4_false[i])
 
-    #各字典按照值的大小进行排序得到相应元祖，然后转换成DataFrame导出到csv
+    #各字典按照值的大小进行排序得到相应元祖，然后转换为DataFrame并导出到CSV，最后绘图
     # set2 = sorted(set2.items(), key=lambda item:item[1], reverse = True)
     # df_2 = pd.DataFrame(set2)
     # df_2.to_csv("data_words_2.csv")
@@ -174,27 +194,12 @@ def data_analyse(set2, set3, set4):
     # words_2.plot(kind = 'bar')
     # plt.title("words_2")
     # plt.show()
-    #
-    # set3 = sorted(set3.items(), key=lambda item:item[1], reverse = True)
-    # df_3 = pd.DataFrame(set3)
-    # df_3.to_csv("data_words_3.csv")
-    # words_3 = df_3.iloc[0:15]
-    # words_3.plot(kind = 'bar')
-    # plt.title("words_3")
-    # plt.show()
-    #
-    # set4 = sorted(set4.items(), key=lambda item:item[1], reverse = True)
-    # df_4 = pd.DataFrame(set4)
-    # df_4.to_csv("data_words_4.csv")
-    # words_4 = df_4.iloc[0:15]
-    # words_4.plot(kind = 'bar')
-    # plt.title("words_4")
-    # plt.show()
 
 if __name__ == "__main__":
     print("读取数据并进行预处理")
     data, length = get_data()
     print("进行分词处理")
-    data, set2, set3, set4 = word_cut_function(data, length)
-    data_analyse(set2, set3, set4)
+    data, set2, set3_true, set3_false, set4_true, set4_false = word_cut_function(data, length)
+    print("结果数据处理")
+    data_analyse(set2, set3_true, set3_false, set4_true, set4_false)
     data.to_csv("data_treat.csv")
