77c5a3134ccc9c0c0280613c6e61948b4473af21
MarsPain <1434579002@qq.com>
2018-06-19 17:04:48 +0800

    在中药测试数据上测试kmeans方法

--- medicine_similarity/medicine_cluster.py
+++ medicine_similarity/medicine_cluster.py
@@ -1,23 +1,33 @@
 import numpy as np
 from kmodes.kmodes import KModes
+from sklearn.cluster import KMeans
 import pandas as pd
 import numpy as np
 
-def get_data():
-    data = pd.read_csv("feature_vector.csv")
+def get_data(filename):
+    data = pd.read_csv(filename)
     # print(data.info())
     # print(data)
     data = np.asarray(data) #dataframe转array
     data = data[:,1:]
-    print(data, data.shape)
+    # print(data, data.shape)
     return data
 
 def cluster_kmodes():
-    data = get_data()
-    km = KModes(n_clusters=20, init="Huang", n_init=10, verbose=1)
-    clusters = km.fit_predict(data)
+    data = get_data("feature_vector.csv")
+    kmodes = KModes(n_clusters=30, init="Huang", n_init=10, verbose=1)
+    clusters = kmodes.fit_predict(data)
     print(clusters) #输出每个样本的类别
-    print(km.cluster_centroids_)    #输出聚类结束后的簇中心
+    print(kmodes.cluster_centroids_)    #输出聚类结束后的簇中心
+
+def cluster_kmeans():
+    data = get_data("feature_vector_pca.csv")
+    kmeans = KMeans(n_clusters=30)
+    clusters = kmeans.fit_predict(data)
+    print(kmeans.inertia_)
+    print(clusters)
+    print(kmeans.cluster_centers_)
 
 if __name__ == "__main__":
-    cluster_kmodes()
+    # cluster_kmodes()
+    cluster_kmeans()
