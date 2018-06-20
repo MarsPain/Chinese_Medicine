2da94f53094541d98db83f1121c0431b084733d6
MarsPain <1434579002@qq.com>
2018-06-20 17:36:36 +0800

    对聚类结果进行可视化

--- medicine_similarity/medicine_cluster.py
+++ medicine_similarity/medicine_cluster.py
@@ -33,10 +33,11 @@ def cluster_kmeans(n_clusters):
     # print("聚类性能", kmeans.inertia_)
     # print("每个样本点所属类别索引", clusters)
     # print("簇中心", kmeans.cluster_centers_)
-    visual_clusters(n_clusters, data, clusters)
+    visual_cluster(n_clusters, data, clusters)
 
 def visual_data(data):
     length = len(data[0])
+    # print(type(data))
     x_length, y_length, z_length = length//3, 2*(length//3), 3*(length//3)
     #各个轴可以是同样长度的数组，不必须是单一的数值，尚不清楚数组如何转换为指定坐标的值
     x, y, z = data[:, :x_length], data[:, x_length:y_length], data[:, y_length:z_length]
@@ -47,7 +48,7 @@ def visual_data(data):
     plt.show()
 
 #将聚类后的样本分别存入各标签数组中
-def visual_clusters(n_clusters, data, clusters):
+def clusters_label_class(n_clusters, data, clusters):
     data_labeled_lists = []
     # print(data, clusters)
     length = len(data)
@@ -55,14 +56,33 @@ def visual_clusters(n_clusters, data, clusters):
         data_labeled_lists.append([])
     # print(data_labeled_lists)
     for i in range(length):
-        data_labeled_lists[clusters[i]].append(data[i])
+        data_labeled_lists[clusters[i]].append(data[i].tolist())
+    #列表转数组
+    for i in range(n_clusters):
+        data_labeled_lists[i] = np.asarray(data_labeled_lists[i])
+    data_labeled_lists = np.asarray(data_labeled_lists)
     print(data_labeled_lists)
     #验证是否所有样本均归类
     num_count = 0
     for data_labeled_list in data_labeled_lists:
         for data_labeled in data_labeled_list:
             num_count += 1
-    print("Yes!" if num_count==length else "Error!")
+    # print("Yes!" if num_count==length else "Error!")
+    return data_labeled_lists
+
+def visual_cluster(n_clusters, data, clusters):
+    data_labeled_lists = clusters_label_class(n_clusters, data, clusters)
+    length = len(data[0])
+    x_length, y_length, z_length = length//3, 2*(length//3), 3*(length//3)
+    colors = ['b','g','r','k','c','m','y','#e24fff','#524C90','#845868']
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    for i in range(n_clusters):
+        x, y, z = data_labeled_lists[i][:, :x_length], data_labeled_lists[i][:, x_length:y_length], \
+                  data_labeled_lists[i][:, y_length:z_length]
+        # print(x, y, z)
+        ax.scatter(x, y, z, marker="o", c=colors[i])
+    plt.show()
 
 if __name__ == "__main__":
     n_clusters = 10
