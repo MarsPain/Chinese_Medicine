21e612fa24f330515377a35cf36b62f31f8b81f0
MarsPain <1434579002@qq.com>
2018-08-04 11:22:59 +0800

    some change

--- feature_engineering/other_data_manage.py
+++ feature_engineering/other_data_manage.py
@@ -112,44 +112,50 @@ import re
 #     f_target.write(string)
 
 # 对总药物数据集进行分析，主要对性味归经功效特征词的数量以及词频进行统计
-path = "data/data_all_v4.csv"
-data = pd.read_csv(path)
-print(data.info())
-data = data.fillna("missing")
-print(data.info())
-length = data.shape[0]
-# 分割数据
-for i in range(length):
-    l = data["Taste"].loc[i]
-    data["Taste"].loc[i] = re.split("、", l)
-    l = data["Type"].loc[i]
-    data["Type"].loc[i] = re.split("、", l)
-    data["Function"].loc[i] = re.split("[，、；]", data["Function"].loc[i])
-# 统计数据
-taste_dict = {}
-type_dict = {}
-function_dict = {}
-for i in range(length):
-    for taste in data["Taste"].loc[i]:
-        print(taste)
-        taste_dict[taste] = taste_dict[taste]+1 if taste in taste_dict else 1
-    for type in data["Type"].loc[i]:
-        print(type)
-        # if len(taste) == 2 or len(taste) == 3 or len(taste) == 4:
-        type_dict[type] = type_dict[type]+1 if type in type_dict else 1
-    for function in data["Function"].loc[i]:
-        print(function)
-        if len(function) == 2 or len(function) == 3 or len(function) == 4:
-            function_dict[function] = function_dict[function]+1 if function in function_dict else 1
-taste_dict_sorted = sorted(taste_dict.items(), key=lambda x: (-x[1], x[0]))  # 根据特征出现频次进行排序
-type_dict_sorted = sorted(type_dict.items(), key=lambda x: (-x[1], x[0]))
-function_dict_sorted = sorted(function_dict.items(), key=lambda x: (-x[1], x[0]))
-print("taste_dict_sorted:", len(taste_dict_sorted), taste_dict_sorted)
-print("type_dict_sorted:", len(type_dict_sorted), type_dict_sorted)
-print("function_dict_sorted:", len(function_dict_sorted), function_dict_sorted)
+# path = "data/data_all_v4.csv"
+# data = pd.read_csv(path)
+# print(data.info())
+# data = data.fillna("missing")
+# print(data.info())
+# length = data.shape[0]
+# # 分割数据
+# for i in range(length):
+#     l = data["Taste"].loc[i]
+#     data["Taste"].loc[i] = re.split("、", l)
+#     l = data["Type"].loc[i]
+#     data["Type"].loc[i] = re.split("、", l)
+#     data["Function"].loc[i] = re.split("[，、；]", data["Function"].loc[i])
+# # 统计数据
+# taste_num = 0
+# taste_dict = {}
+# type_num = 0
+# type_dict = {}
+# function_num = 0
+# function_dict = {}
+# for i in range(length):
+#     for taste in data["Taste"].loc[i]:
+#         # print(taste)
+#         taste_num += 1
+#         taste_dict[taste] = taste_dict[taste]+1 if taste in taste_dict else 1
+#     for type in data["Type"].loc[i]:
+#         # print(type)
+#         type_num += 1
+#         # if len(taste) == 2 or len(taste) == 3 or len(taste) == 4:
+#         type_dict[type] = type_dict[type]+1 if type in type_dict else 1
+#     for function in data["Function"].loc[i]:
+#         # print(function)
+#         function_num += 1
+#         if len(function) == 2 or len(function) == 3 or len(function) == 4:
+#             function_dict[function] = function_dict[function]+1 if function in function_dict else 1
+# taste_dict_sorted = sorted(taste_dict.items(), key=lambda x: (-x[1], x[0]))  # 根据特征出现频次进行排序
+# type_dict_sorted = sorted(type_dict.items(), key=lambda x: (-x[1], x[0]))
+# function_dict_sorted = sorted(function_dict.items(), key=lambda x: (-x[1], x[0]))
+# print("taste_dict_sorted:", taste_num, len(taste_dict_sorted), taste_dict_sorted)
+# print("type_dict_sorted:", type_num, len(type_dict_sorted), type_dict_sorted)
+# print("function_dict_sorted:", function_num, len(function_dict_sorted), function_dict_sorted)
 
 # 对人工标注的分词结果进行统计
-# path = "data/dict_function/set3_true_dict_labeld.txt"
+# path = "data/dict_function/set4_true_dict_labeld.txt"
 # with open(path, "r", encoding="utf-8") as f:
 #     lines = f.readlines()
 #     true_count, false_count = 0, 0
