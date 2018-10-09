ff24fa1741340180c738922ad201bbff1e37241b
MarsPain <1434579002@qq.com>
2018-10-09 14:41:37 +0800

    some change

--- medicine_similarity/function_cluster_entropy.py
+++ medicine_similarity/function_cluster_entropy.py
@@ -11,8 +11,8 @@ from medicine_similarity.cluster import duplicate_removal, del_by_correlation, c
 medicine_path = 'data/data_labeld_kmodes.csv'    # 药物数据的路径（该药物数据已根据性味归经的聚类结果打上标签）
 thesaurus_path = "../data/function_tongyici.txt"  # 功效同义词字典的路径
 correlation_path = 'data/correlation.csv'   # 保存互信息的文件路径
-max_relatives_nums = 21  # 最大的亲友团数量
-min_relatives_nums = 20  # 最小的亲友团数量
+max_relatives_nums = 15  # 最大的亲友团数量
+min_relatives_nums = 10  # 最小的亲友团数量
 group_all_path = "data/group_all.csv"
 group_best_path = "data/group_best.csv"
 function_to_medicine_path = "data/function_to_medicine.pkl"  # 保存功效团到相似药物的映射
@@ -164,7 +164,7 @@ class ClusterEntropy:
         # 人工审核找到信息利用率最高的亲友团数量，即最佳的亲友团数量，然后以该亲友团数量下的结果作为最佳结果
         group_best_name = []
         group_best = []
-        for i in range(21, 22):
+        for i in range(12, 13):
             group_path = os.path.join("data", "group"+str(i)+".csv.pkl")
             group_best_name.append("group"+str(i))
             group_best.append(group_clean(group_path))
